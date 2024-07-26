18feb484fbbb8b73e8d6fa9908d18fea9722cd4c
Tamara Silkina <silkinats@mail.ru>
2024-07-26 14:37:07 +0300

    add: branch lesson1

---
+++ typeof.js
@@ -0,0 +1,19 @@
+typeof("maxim")
+typeof "Maxim"
+console.log(typeof "Maxim");
+console.log(typeof 5);
+console.log(typeof false);
+console.log(typeof Symbol("id"));
+console.log(typeof undefined);
+console.log(typeof 100n);
+console.log(typeof console);
+
+//Исключения
+
+console.log(typeof null); //object
+
+// console.log(typeof []); //object
+
+// console.log(typeof new Date()); //object
+console.log(typeof console.log); //function, which is not existing in JavaScript
+
