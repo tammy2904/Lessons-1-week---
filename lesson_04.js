4d0ed8f10f73b3c61e328b420be3dd8bdbd90cab
Tamara Silkina <silkinats@mail.ru>
2024-07-28 17:19:29 +0300

    add: lesson_04

---
+++ lesson_04.js
@@ -0,0 +1,34 @@
+// Примеры использования базовых операторов в JavaScript
+let counter = -5;
+console.log(counter); // -5
+console.log('+', 2+2);
+console.log('-', 10 - 5);
+console.log('*', 2*2);
+console.log('/', 10/2);
+console.log(2 + 2*2);
+console.log('**', 2 ** 3); // возведение в степень
+console.log('%', 10 % 3); // остаток от деления
+// Инкремент и декремент
+// let cupsOfCoffee = 5;
+// cupsOfCoffee = cupsOfCoffee + 1;
+// cupsOfCoffee+= 1;  еще один эквивалентный способ
+// cupsOfCoffee = cupsOfCoffee - 2; // cupsOfCoffee-= 2;  еще один эквивалентный способ
+// cupsOfCoffee -=2;
+// cupsOfCoffee++; // cupsOfCoffee+= 1;  еще один эквивалентный способ, добавит к счетчику 1
+// ++cupsOfCoffee; // cupsOfCoffee+= 1;  ещё один эквивалентный способ, добавит к счетчику
+
+
+// console.log('cupsOfCoffee', cupsOfCoffee); 
+let cupsOfCoffee = 5 
+console.log('1', cupsOfCoffee++); 
+console.log('2', ++cupsOfCoffee); // разница между постфиксным и префиксным инкрементом указывается на экране, но в реальности они не изменяют
+
+// let value = 10;
+// // value = value * 5;
+// value *= 5;
+// console.log('value', value); // 50
+let value = 10;
+// value /= 2; equals value = value / 2;
+console.log('value', value); // 5
+
+
