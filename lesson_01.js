c68bfe33dbe520ed22713bede1339c01585f8d69
Tamara Silkina <silkinats@mail.ru>
2024-07-30 12:42:48 +0300

    fix: lesson_01

--- lesson_01.js
+++ lesson_01.js
@@ -9,7 +9,7 @@
  // Например, когда мы копируем массив, мы не создаем новый объект, а создаем новую ссылку на тот же массив, что позволяет изменять его внутренности.
  // Однако, при создании нового объекта из существующего, мы создаем новый объект, а не новую ссылку на тот же объект.
  // Это позволяет избежать изменения внутренностей объектов при использовании копирования. 
-// -Примитивые данные: string, number, boolean, null, undefined, bigint, symbol.
+// -Примитивные данные: string, number, boolean, null, undefined, bigint, symbol.
 
  //-Не примитивы; объекты, массивы, функции, карты, множеств 
 
@@ -17,12 +17,12 @@
 //1. string
 const favoriteDrink = "Coffee";
 console.log(favoriteDrink);
-console.log(`My favorite drink is ${favoriteDrink}`);
+console.log(`My favorite drink is ${favoriteDrink}.`);
 
-//2. number
+ //2. number
 const numberOfCups = 5;                              
-console.log(numberOfCups);
-console.log(`I have ${numberOfCups} cups of ${favoriteDrink}`); 
+console.log(`Number of cups for a day is ${numberOfCups}.`);
+console.log(`I have ${numberOfCups} cups of ${favoriteDrink} every day.`); 
 
 //3. boolean
 const isColdDrink = true;
@@ -35,29 +35,27 @@ console.log(studentFavoriteDrink);
 console.log(`My favorite drink is ${studentFavoriteDrink}`);
 
 //5. undefined - неинициализированное значение. значение не было присвоено
-//let carOwner;
+// let carOwner;
 
 let carOwner = undefined;
-console.log(`Car owner is ${carOwner}`);
 console.log(carOwner);
-
+console.log(`Car owner is ${carOwner}`);
+ 
 //6. object - объект
 const drink = {
     favoriteDrink: "Coffee",
     numberOfCups: 5,
-    isColdDrink: true
+    isColdDrink: true}
     // key: value
-};
-console.log(drink);
+    console.log( `My favorite drink is ${favoriteDrink}. And I have ${numberOfCups} every day. The ${favoriteDrink} is cold or warm? It is cold, ${isColdDrink}.`);
+    console.log(drink); // {favoriteDrink: 'Coffee', numberOfCups: 5, isColdDrink: true} /console.log
 
 // 7. symbol
- const id = Symbol("id");
- console.log(id);
+const id = Symbol("id");
+console.log(id);
 
- // 8. bigint - для работы с большими числами
- /*const bigNumber = 12345678901234567890n;
- console.log(bigNumber);
- console.log(typeof bigNumber); // bigint*/
-
-const bigIntNumber = 10n;
+// 8. bigint - для работы с большими числами 
+const bigIntNumber = BigInt(10); //or bigIntNumber = 10n;
+console.log(bigIntNumber);
+console.log(typeof bigIntNumber);
 console.log(bigIntNumber);
---
+++ lesson_11.js
@@ -0,0 +1,8 @@
+// Debugging with console.log
+
+let = sum;
+const numberOfElements = 10;
+for (let i = 1; i < numberOfElements; i +=1) {
+sum += i }
+const theHalfOfSum = sum / 2;
+console.log('theHalfOfSum', theHalfOfSum);
