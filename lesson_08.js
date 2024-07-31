11eac5e5a9a3b823a797816c0b6eef71ce054c95
Tamara Silkina <silkinats@mail.ru>
2024-07-31 20:56:33 +0300

    add: lesson-08

---
+++ lesson_08.js
@@ -0,0 +1,33 @@
+// for
+// while
+// do while
+
+
+
+// for(начальное значение; условие; шаг) {}
+// for(let i = 0; i < 10; i += 1) {
+//     console.log('i =', i);
+// }
+// for (let i = 10; i >= 0; i -= 1) {
+//     console.log('i =', i);
+// }
+
+// while
+
+// while (условие) {}
+// let i = 0; 
+// while (i<10) {
+//     console.log('i', i);
+//     i += 1;
+// }
+
+// do while
+// do {
+
+// }
+let i = 0;
+do {
+    console.log('i', i);
+    i += 1;
+}
+while (i <= 5);
