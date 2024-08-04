ab5439112cd9fbd824b6dc39905344377ddc3714
Tamara Silkina <silkinats@mail.ru>
2024-08-04 20:29:44 +0300

    add: lesson18

--- lesson_17.js
+++ lesson_17.js
@@ -1,13 +1,15 @@
 const salariesOfDevelopers = [400,500, 600, 2000, 350];
 
-// Добавление элемента в конец массива
+// Добавление элемента в конец массива .push ()
 const newSeniorDeveloperSalary = 5000;
 salariesOfDevelopers.push(newSeniorDeveloperSalary, 5001, 5002, 5003);
 console.log('salariesOfDevelopers', salariesOfDevelopers);
-// Добавление элемента в начало массива
+
+// Добавление элемента в начало массива .unshift ()
 salariesOfDevelopers.unshift(100, 101, 102, 103);
 console.log('salariesOfDevelopers', salariesOfDevelopers);
-// Удаление элементов из массива
+
+// Удаление элементов из массива shift () -from start, и pop () from the end
 const firstRemovedElement = salariesOfDevelopers.shift();
 salariesOfDevelopers.shift();
 const lastRemovedElement = salariesOfDevelopers.pop();
@@ -16,6 +18,7 @@ console.log('salariesOfDevelopers', salariesOfDevelopers); //100, 101 are remove
 console.log('firstRemovedElement', firstRemovedElement);
     salariesOfDevelopers.shift(); //100
 console.log('lastRemovedElement', lastRemovedElement); //5003
+
 // Изменение существующего элемента массива
 const salariesOfDevelopers2 = [400,500, 600, 2000, 350];
 
